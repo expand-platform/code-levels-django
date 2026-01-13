e056f2afb9cbf90af29325d8761b30beb94750fc
Ca-tt <damir96.lukyanenko@gmail.com>
2026-01-13 18:38:08 +0200

    revert base path

--- code_levels/settings/prod.py
+++ code_levels/settings/prod.py
@@ -3,7 +3,6 @@ from code_levels.settings.allauth.prod import *
 from dj_database_url import parse
 
 DEBUG = False
-BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / "staticfiles"
 ROOT_URLCONF = "code_levels.urls"
 
