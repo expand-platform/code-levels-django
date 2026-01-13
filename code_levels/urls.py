8f71204491c87c98de5cb916b48e725eda951080
Ca-tt <damir96.lukyanenko@gmail.com>
2026-01-13 20:09:23 +0200

    add healthcheck endpoint returning "ok"

--- code_levels/urls.py
+++ code_levels/urls.py
@@ -2,15 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
 # from store.config.config import WebsiteSettings
 
 # handler404 = "platform_web.views.not_found_404"
+
+def healthcheck(request):
+    return HttpResponse("ok")
 
 urlpatterns = [
     path("", include("platform_web.urls")),
     path("cp/", admin.site.urls),
     path("account/", include(("allauth.urls"))),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("healthz/", healthcheck),
 ]
 
 # ? development
