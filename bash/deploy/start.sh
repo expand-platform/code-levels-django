5476d52c7c440ce92d7ac0e586acf42ea6328c60
Ca-tt <damir96.lukyanenko@gmail.com>
2026-01-13 17:55:27 +0200

    update deploy scripts

--- bash/deploy/start.sh
+++ bash/deploy/start.sh
@@ -2,7 +2,10 @@
 set -e
 
 python manage.py check --deploy --settings=code_levels.settings.prod
-python manage.py collectstatic --settings=code_levels.settings.prod --noinput
 
 python manage.py makemigrations --settings=code_levels.settings.prod
 python manage.py migrate --settings=code_levels.settings.prod
+
+python manage.py collectstatic --settings=code_levels.settings.prod --noinput
+
+exec gunicorn code_levels.wsgi:application --bind 0.0.0.0:$PORT
